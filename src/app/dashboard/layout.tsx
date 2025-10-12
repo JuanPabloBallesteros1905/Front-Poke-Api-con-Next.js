667a39c0517fe6f945daca1664af35d985945579
JuanPabloBallesteros1905 <juanpablo-1905@hotmail.com>
2025-10-12 13:39:51 -0500

    newBackUp

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,30 +1,17 @@
 import { SideBar } from "./components/SideBar";
 
-
 export default function DashboardLayout({
-    children
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
+  return (
+    <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+      <div className="flex">
+        <SideBar />
 
-        <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
-            <div className="flex">
-
-
-                <SideBar />
-
-                <div className="p-2 w-full text-slate-900" >
-
-                    {children}
-
-                </div>
-
-
-
-            </div>
-        </div>
-
-
-    );
+        <div className="p-2 w-full text-slate-900">{children}</div>
+      </div>
+    </div>
+  );
 }
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -3,94 +3,29 @@ import { PokeResponse } from "@/app/pokemons/interfaces/pokeResponse";
 import { SimplePokemon } from "@/app/pokemons/interfaces/simple-pokemons";
 import Image from "next/image";
 
-
-
-
 const getPokemon = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokeResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
-
-
-
-
-
-  const data: PokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then(
-    res => res.json()
-
-  );
-
-
-  const pokemons: SimplePokemon[] = data.results.map(pokeModal => ({
-    id: pokeModal.url.split('/').at(-2)!,
+  const pokemons: SimplePokemon[] = data.results.map((pokeModal) => ({
+    id: pokeModal.url.split("/").at(-2)!,
     name: pokeModal.name,
   }));
 
-
-
-
-
-
-
-  return pokemons
-
-
-
-
-
-
-
-
-
-}
-
-
+  return pokemons;
+};
 
 export default async function PokemonPage() {
-
-
-
   const pokemon: SimplePokemon[] = await getPokemon(100, 0);
 
-
-
-
-
-
-
-
   return (
-
-
-
-
-
-
-
-
-    <div className="flex flex-col" >
-
-      <div className="flex flex-wrap gap-10 justify-center items-center " >
-
-
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 justify-center items-center ">
         <span>Listado estatico de pokemons</span>
 
-
         <PokemonGrid pokemon={pokemon} />
-
-
-
-
-
-
-
-
-
-
       </div>
-
-
-
-
-
     </div>
-  )
+  );
 }
